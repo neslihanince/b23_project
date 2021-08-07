76afbe5bdb5bad536698fe3fa8e647529ab1c3fa
neslihanince <neslihanince94@gmail.com>
2021-08-07 00:13:26 -0400

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,9 @@ public class GitCool {
     // B23 IS LEARNING GIT
     // B23 IS A SPECIAL NUMBER
     // B23 IS MAKING SOME SENSE NOW
+
+    // We have added our GitHub account under intelliJ
+    // make sure to click apply and click on to save
+
+
 }

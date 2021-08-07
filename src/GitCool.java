4433ffa61b2d00a9ce82828ecb428e997709dafa
neslihanince <neslihanince94@gmail.com>
2021-08-07 00:16:48 -0400

    added github account ready to push

--- src/GitCool.java
+++ src/GitCool.java
@@ -6,5 +6,11 @@ public class GitCool {
     // We have added our GitHub account under intelliJ
     // make sure to click apply and click on to save
 
+    // er were able to successfully shared project under our guthub account
+    // Share Project On github happen only one time per project
+    // it combines the 3 actions below
+    // Create remote repository under your account
+    // Push local commits to remote repo
+
 
 }
